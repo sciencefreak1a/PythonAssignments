668d257106ae707fc23ed9d9c6841b89d541de66
sciencefreak1a <ajit1a@gmail.com>
2022-12-06 20:40:53 +0530

    initial commit

---
+++ python programming assignment 3.py
@@ -0,0 +1,13 @@
+
+number = int(input("Enter number : "))
+if number < 0 :
+    print("The number is negative.")
+
+elif number > 0 :
+    print("The number is positive")
+
+elif number == 0 :
+    print("The number is zero")
+
+else :
+    print("Wrong Input")
